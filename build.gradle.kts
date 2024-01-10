27d117259ffeffa9900b73308e2da6db1a515703
sursindmitry <dmitrijsursin@gmail.com>
2024-01-10 23:55:41 +0700

    #2: Check build-and-push-image in Github Actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,9 +26,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:$springBootVersion")
     implementation("org.postgresql:postgresql:$postgresqlVersion")
     implementation("org.liquibase:liquibase-core:$liquibaseVersion")
-    
-    compileOnly("org.projectlombok:lombok:1.18.30")
-    annotationProcessor("org.projectlombok:lombok:1.18.30")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test:$springBootVersion")
 }
