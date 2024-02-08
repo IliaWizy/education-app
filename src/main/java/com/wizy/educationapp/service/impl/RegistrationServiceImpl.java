730fdc9e455096338f2ed1857f22c7a392dd82db
Dmitry Sursin <dmitrijsursin@gmail.com>
2024-02-08 13:41:08 +0700

    #19: Set default values for user

--- src/main/java/com/wizy/educationapp/service/impl/RegistrationServiceImpl.java
+++ src/main/java/com/wizy/educationapp/service/impl/RegistrationServiceImpl.java
@@ -27,9 +27,9 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     User user = userMapper.toEntity(requestDto);
     user.setPassword(passwordEncoder.encode(requestDto.password()));
-    user.setAccountExpired(true);
-    user.setAccountLocked(true);
-    user.setCredentialExpired(true);
+    user.setAccountExpired(false);
+    user.setAccountLocked(false);
+    user.setCredentialExpired(false);
     user.setActive(false);
     user.setRoles(Collections.singleton(Role.ROLE_USER));
 
